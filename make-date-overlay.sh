c2c8cd265a08185e5d253b5efd7e609e880680ee
Andrey Stepachev <octo47@gmail.com>
2011-05-18 00:19:33 +0400

    initial

---
+++ make-date-overlay.sh
@@ -0,0 +1,12 @@
+if [ \! -f "$1" ]; then
+	echo "usage: make-script file"
+	exit 1
+fi
+videofile=$1
+#labelfile="$(dirname $1)/$(uuidgen).png"
+labelfile="/var/tmp/$(uuidgen).gif"
+
+convert -density 120 -background transparent -fill lightblue  \
+           -strokewidth 1  -stroke white   -undercolor transparent \
+           -size 130x60 -gravity center label:"$(date +%Y-%m-%d --reference="$videofile")" $labelfile
+echo $labelfile
---
+++ recode-mts.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+D=$(dirname "$1")
+B=$(basename "$1" .MTS)
+T="$D/$B.mp4"
+exiftool "-DateTimeOriginal>FileModifyDate" "$1"
+labelfile=$(./make-date-overlay.sh "$1")
+ffmpeg -i "$1" -deinterlace -s hd720 -bf 2 -b 16000k -sameq -ab 256k -y \
+	-vf "movie=$labelfile [wm];[in][wm] overlay=W-w-20:H-h-10:1 [out]" \
+	"$T"
+rm -f $labelfile
+if [ \! -s "$T" ]; then
+	echo "No file created"
+	rm -f "$T"
+	exit 1
+fi
+touch -r "$1" "$D/$B.mp4"
+
